8b5145cb738d17a38643aef913712b46a26722d4
naminsu <naminsoo1020@naver.com>
2025-12-03 18:47:24 +0900

    feat: 에러코드 추가 - 로그인시 이메일 비밀번호 다르면 같은 예외

--- src/main/java/com/popjub/userservice/exception/UserErrorCode.java
+++ src/main/java/com/popjub/userservice/exception/UserErrorCode.java
@@ -11,11 +11,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum UserErrorCode implements BaseErrorCode {
 	/**
+	 * 인증
+	 */
+	INVALID_CREDENTIALS("이메일 또는 비밀번호가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED),
+	/**
 	 * 회원
 	 */
 	DUPLICATE_EMAIL("이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
 	DUPLICATE_NICKNAME("이미 사용 중인 닉네임입니다.", HttpStatus.CONFLICT),
 	CANNOT_CREATE_ADMIN("ADMIN 역할은 생성할 수 없습니다.", HttpStatus.BAD_REQUEST),
+	NOT_FOUND_USER("존재하지 않은 사용자입니다.", HttpStatus.NOT_FOUND),
 	/**
 	 * Validation
 	 */
