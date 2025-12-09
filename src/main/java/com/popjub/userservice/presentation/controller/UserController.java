9b768fe05828129db59d76d7ba0ab0e9575bdab2
naminsu <naminsoo1020@naver.com>
2025-12-09 23:41:12 +0900

    feat: 내정보 조회를 위한 presentation 영역 코드 추가 - 내정보 조회(GET /api/v1/users/me) - SearchUserDetailResponse로 응답

--- src/main/java/com/popjub/userservice/presentation/controller/UserController.java
+++ src/main/java/com/popjub/userservice/presentation/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.popjub.userservice.presentation.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.popjub.common.annotation.CurrentUser;
+import com.popjub.common.annotation.RoleCheck;
 import com.popjub.common.enums.SuccessCode;
+import com.popjub.common.enums.UserRole;
 import com.popjub.common.response.ApiResponse;
+import com.popjub.userservice.application.dto.result.SearchUserDetailResult;
 import com.popjub.userservice.application.service.UserService;
 import com.popjub.userservice.presentation.dto.request.UpdateNotificationUrlsRequest;
+import com.popjub.userservice.presentation.dto.response.SearchUserDetailResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,5 +40,13 @@ public class UserController {
 			.message("알림받을 디스코드/슬랙 url 등록에 성공했습니다.")
 			.code(SuccessCode.OK)
 			.build();
+	}
+
+	@GetMapping("/me")
+	@RoleCheck({UserRole.USER, UserRole.STORE_MANAGER, UserRole.ADMIN})
+	public ApiResponse<SearchUserDetailResponse> getMyProfile(@CurrentUser Long userId) {
+		SearchUserDetailResult result = userService.getUserById(userId);
+		SearchUserDetailResponse response = SearchUserDetailResponse.from(result);
+		return ApiResponse.of("내 정보 조회에 성공했습니다.", response);
 	}
 }
