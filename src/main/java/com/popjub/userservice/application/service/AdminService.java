5a217d502e40e1996d745bb986f02b884a02ee56
naminsu <naminsoo1020@naver.com>
2025-12-10 14:38:51 +0900

    feat: 유저전체 조회를 위한 application 영역 코드 추가 - getAllUsers() 구현 - SearchUserResult로 반환

--- src/main/java/com/popjub/userservice/application/service/AdminService.java
+++ src/main/java/com/popjub/userservice/application/service/AdminService.java
@@ -1,11 +1,14 @@
 package com.popjub.userservice.application.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.popjub.userservice.application.dto.command.CreateUserCommand;
 import com.popjub.userservice.application.dto.result.SearchUserDetailResult;
+import com.popjub.userservice.application.dto.result.SearchUserResult;
 import com.popjub.userservice.domain.entity.User;
 import com.popjub.userservice.domain.entity.UserRole;
 import com.popjub.userservice.domain.repository.UserRepository;
@@ -59,5 +62,11 @@ public class AdminService {
 			.orElseThrow(() -> new UserCustomException(UserErrorCode.NOT_FOUND_USER));
 
 		return SearchUserDetailResult.from(user);
+	}
+
+	public Page<SearchUserResult> getAllUsers(Pageable pageable) {
+		Page<User> userPage = userRepository.findAll(pageable);
+
+		return userPage.map(SearchUserResult::from);
 	}
 }
