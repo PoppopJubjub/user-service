4874ec59f73e6f408629331c89c433acf476b31e
naminsu <naminsoo1020@naver.com>
2025-12-10 14:37:40 +0900

    feat: 유저전체 조회를 위한 presentation 영역 코드 추가 - 유저전체조회[ADMIN] (GET /api/v1/admin/users) - SearchUserResponse 추가

--- src/main/java/com/popjub/userservice/presentation/controller/AdminController.java
+++ src/main/java/com/popjub/userservice/presentation/controller/AdminController.java
@@ -1,5 +1,11 @@
 package com.popjub.userservice.presentation.controller;
 
+import static com.popjub.common.enums.UserRole.*;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,13 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.popjub.common.annotation.RoleCheck;
-import com.popjub.common.enums.UserRole;
 import com.popjub.common.response.ApiResponse;
+import com.popjub.common.response.PageResponse;
 import com.popjub.userservice.application.dto.result.SearchUserDetailResult;
+import com.popjub.userservice.application.dto.result.SearchUserResult;
 import com.popjub.userservice.application.service.AdminService;
 import com.popjub.userservice.domain.entity.User;
 import com.popjub.userservice.presentation.dto.request.CreateUserRequest;
 import com.popjub.userservice.presentation.dto.response.SearchUserDetailResponse;
+import com.popjub.userservice.presentation.dto.response.SearchUserResponse;
 import com.popjub.userservice.presentation.dto.response.SignUpUserResponse;
 
 import jakarta.validation.Valid;
@@ -27,9 +35,7 @@ public class AdminController {
 
 	private final AdminService adminService;
 
-	/**
-	 * TODO: RoleCheck ADMIN
-	 */
+	@RoleCheck(ADMIN)
 	@PostMapping("/users")
 	public ApiResponse<SignUpUserResponse> createUser(
 		@Valid @RequestBody CreateUserRequest request
@@ -40,7 +46,7 @@ public class AdminController {
 		return ApiResponse.of("회원이 생성되었습니다.", response);
 	}
 
-	@RoleCheck(UserRole.ADMIN)
+	@RoleCheck(ADMIN)
 	@GetMapping("/users/{userId}")
 	public ApiResponse<SearchUserDetailResponse> getUserDetailByAdmin(
 		@PathVariable Long userId
@@ -49,5 +55,21 @@ public class AdminController {
 		SearchUserDetailResponse response = SearchUserDetailResponse.from(result);
 
 		return ApiResponse.of("특정사용자의 정보 조회에 성공했습니다.", response);
+	}
+
+	@RoleCheck(ADMIN)
+	@GetMapping("/users")
+	public ApiResponse<PageResponse<SearchUserResponse>> getAllUsers(
+		@PageableDefault(
+			size = 10,
+			sort = "createdAt",
+			direction = Sort.Direction.DESC
+		) Pageable pageable
+	) {
+		Page<SearchUserResult> resultPage = adminService.getAllUsers(pageable);
+		Page<SearchUserResponse> responsePage = resultPage.map(SearchUserResponse::from);
+		PageResponse<SearchUserResponse> pageResponse = PageResponse.from(responsePage);
+
+		return ApiResponse.of("유저 전체조회에 성공했습니다", pageResponse);
 	}
 }
---
+++ src/main/java/com/popjub/userservice/presentation/dto/response/SearchUserResponse.java
@@ -0,0 +1,27 @@
+package com.popjub.userservice.presentation.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.popjub.userservice.application.dto.result.SearchUserResult;
+
+import lombok.Builder;
+
+@Builder
+public record SearchUserResponse(
+	Long userId,
+	String email,
+	String nickname,
+	String userName,
+	LocalDateTime createdAt
+) {
+
+	public static SearchUserResponse from(SearchUserResult searchUserResult) {
+		return SearchUserResponse.builder()
+			.userId(searchUserResult.userId())
+			.email(searchUserResult.email())
+			.nickname(searchUserResult.nickname())
+			.userName(searchUserResult.userName())
+			.createdAt(searchUserResult.createdAt())
+			.build();
+	}
+}
