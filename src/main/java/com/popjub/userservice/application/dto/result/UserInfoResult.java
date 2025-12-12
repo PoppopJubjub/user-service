5835c38f5661c6997ef40732f193b1861c64acf2
naminsu <naminsoo1020@naver.com>
2025-12-12 11:56:46 +0900

    refactor: 내부api 수정 - /internal/users/{userId} 경로 변경 - dto userName 추가

--- src/main/java/com/popjub/userservice/application/dto/result/UserInfoResult.java
+++ src/main/java/com/popjub/userservice/application/dto/result/UserInfoResult.java
@@ -7,12 +7,14 @@ import lombok.Builder;
 @Builder
 public record UserInfoResult(
 	Long userId,
+	String userName,
 	String discordUrl,
 	String slackUrl
 ) {
 	public static UserInfoResult from(User user) {
 		return UserInfoResult.builder()
 			.userId(user.getUserId())
+			.userName(user.getUserName())
 			.discordUrl(user.getDiscordUrl())
 			.slackUrl(user.getSlackUrl())
 			.build();
