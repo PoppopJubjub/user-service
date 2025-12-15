ce5efa0556372721f0f027da33a40337113e94e1
naminsu <naminsoo1020@naver.com>
2025-12-15 10:40:27 +0900

    move: config 패키지 인프라계층으로 이동

--- src/main/java/com/popjub/userservice/infrastructure/config/JwtProperties.java
+++ src/main/java/com/popjub/userservice/infrastructure/config/JwtProperties.java
@@ -1,4 +1,4 @@
-package com.popjub.userservice.config;
+package com.popjub.userservice.infrastructure.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
